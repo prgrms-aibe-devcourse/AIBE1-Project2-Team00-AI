28f942f9d365639b13cf03a0714e15cc4b78a35c
qus0in <qus0in@gmail.com>
2025-04-28 11:51:40 +0900

    CORS 적용

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 const port = 3000
 
 app.get('/', (req, res) => {
